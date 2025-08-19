11a259ca0585075e32ee6356956874e797cc778e
MosefnU <moiseveldema@gmail.com>
2025-08-19 13:25:38 +0200

    comments and small fix in station name

--- __main__.py
+++ __main__.py
@@ -4,11 +4,14 @@ import database_builder
 
 def main():
 
+    # Load and clean the data
     df = pd.read_csv("data.csv", sep=";")
     df = transform_data.clean_data(df)
+
     print(df.head())
     print(df.columns)
     
+    # Place data into SQLite database
     database_builder.pandas_to_sqlite(df, 'transactions', 'transactions.db')
 
 def top_ten_stations(df):
@@ -18,7 +21,7 @@ def top_ten_stations(df):
     :param df: DataFrame containing the data.
     :return: Series with the top ten stations and their counts.
     """
-    return df['STATION '].value_counts().head(10)
+    return df['STATION'].value_counts().head(10)
 
 if __name__ == "__main__":
     main()
