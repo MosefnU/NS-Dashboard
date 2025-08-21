e20ada0a1bff55a9c2c99b4e896b67603aaad143
MosefnU <moiseveldema@gmail.com>
2025-08-21 17:42:34 +0200

    Toon ticket types op dashboard

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 db_file = 'transactions.db'
 #Create header and subheader
 st.title("Kritieke prestatie indicatoren 2025")
-st.subheader("Gebaseerd op barcode scans op 24 maart 2020")
+st.write("Gebaseerd op barcode scans op 24 maart 2020")
 conn = sqlite3.connect(db_file)
 
 # Haal het aantal reizen op
@@ -17,11 +17,14 @@ query = f"""
 """
 stations_df = pd.read_sql_query(query, conn)
 
-# Haal de reizen op
+# Haal de journeys op
 query = f"""
     SELECT * FROM journeys
 """
 journeys_df = pd.read_sql_query(query, conn)
+
+# Haal de transacties op
+transactions_df = pd.read_sql_query("SELECT * FROM transactions", conn)
 conn.close()
 
 # Filter stations buiten NL eruit
@@ -29,14 +32,14 @@ stations_buiten_nl =  ['frankfurt(m)flugh.','frankfurt(main)','köln','mariënbe
 stations_nl_df = stations_df.query(f"STATION != '0' and STATION not in {stations_buiten_nl}")
 
 ## Toon reizen in Nederland
-aantal_check_ins = int(stations_nl_df['passengers_in'].sum())
-aantal_check_outs = int(stations_nl_df['passengers_out'].sum())
-aantal_reizen = max(aantal_check_ins, aantal_check_outs)
-st.subheader("1. Reizen in Nederland")
-reizen, reis_in, reis_uit = st.columns(3)
-reizen.metric(label="Aantal reizen", value=aantal_reizen, delta=None, delta_color="normal")
-reis_in.metric(label="Aantal check-ins", value=aantal_check_ins, delta=None, delta_color="normal")
-reis_uit.metric(label="Aantal check-uits", value=aantal_check_outs, delta=(aantal_check_outs-aantal_check_ins), delta_color="normal")
+# aantal_check_ins = int(stations_nl_df['passengers_in'].sum())
+# aantal_check_outs = int(stations_nl_df['passengers_out'].sum())
+# aantal_reizen = max(aantal_check_ins, aantal_check_outs)
+# st.subheader("0. Reizen in Nederland")
+# reizen, reis_in, reis_uit = st.columns(3)
+# reizen.metric(label="Aantal reizen", value=aantal_reizen, delta=None, delta_color="normal")
+# reis_in.metric(label="Aantal check-ins", value=aantal_check_ins, delta=None, delta_color="normal")
+# reis_uit.metric(label="Aantal check-uits", value=aantal_check_outs, delta=(aantal_check_outs-aantal_check_ins), delta_color="normal")
 
 ## Toon reizen in Nederland 2
 aantal_reizen = journeys_df.shape[0]
@@ -45,7 +48,7 @@ aantal_reizen_NL = journeys_df.query(query).shape[0]
 query = f"(check_in != '0' and check_in in {stations_buiten_nl} and check_uit != '0' and check_uit not in {stations_buiten_nl}) or (check_in != '0' and check_in not in {stations_buiten_nl} and check_uit != '0' and check_uit in {stations_buiten_nl})"
 aantal_reizen_grensoverschrijdend = journeys_df.query(query).shape[0]
 
-st.subheader("Reizen in Nederland en internationaal")
+st.subheader("1. Reizen in Nederland en internationaal")
 reizen, reis_in, reis_uit = st.columns(3)
 reizen.metric(label="Aantal reizen", value=aantal_reizen, delta=None, delta_color="normal")
 reis_in.metric(label="Waarvan binnenland", value=aantal_reizen_NL, delta=None, delta_color="normal")
@@ -63,10 +66,40 @@ ax.set(aspect="equal")
 plt.title("Verdeling reizen")
 
 st.pyplot(fig)
-    
+
+# # Toon de lopers
+st.subheader("3. Verdeling type tickets")
+# splits de ticket types uit.
+transactions_b = transactions_df.query("`TICKET TYPE` == 'B'").shape[0]
+transactions_e = transactions_df.query("`TICKET TYPE` == 'E' or `TICKET TYPE` == 'Enkel'").shape[0]
+transactions_r = transactions_df.query("`TICKET TYPE` == 'R' or `TICKET TYPE` == 'Retour'").shape[0]
+
+e, r, b, = st.columns(3)
+e.metric(label="Enkele reis", value=transactions_e, delta=None, delta_color="normal")
+r.metric(label="Retour", value=transactions_r, delta=None, delta_color="normal")
+b.metric(label="Generiek", value=transactions_b, delta=None, delta_color="normal")
+
+ep, rp, bp, = st.columns(3)
+totaal_tickets = transactions_e + transactions_r + transactions_b
+ep.metric(label="Enkele reis %", value=f"{round(transactions_e / totaal_tickets * 100,2)}%", delta=None, delta_color="normal")
+rp.metric(label="Retour %", value=f"{round(transactions_r / totaal_tickets * 100, 2)}%", delta=None, delta_color="normal")
+bp.metric(label="Generiek %", value=f"{round(transactions_b / totaal_tickets * 100, 2)}%", delta=None, delta_color="normal")
+
+
+values = [transactions_e, transactions_r, transactions_b]
+labels = ['Enkel', 'Retour', 'Generiek']
+colors = [ '#7ef291','#7ec8f2','#f27e99']
+
+fig, ax = plt.subplots()
+ax.pie(values, labels=labels, colors=colors, startangle=90, wedgeprops={'width':0.4})
+ax.set(aspect="equal")
+
+plt.title("Verdering ticket types")
+
+st.pyplot(fig)
 
 # # Toon top 10 drukste stations in Nederland
-st.subheader("2. De drukste stations in Nederland")
+st.subheader("3. De drukste stations in Nederland")
 topx = st.slider("Aantal stations", 1, 100, 10, on_change=None)
 st.write(f"De {topx} drukste stations in Nederland gebaseerd op het aantal reizigers op het aantal check-ins en check-outs.")
 stations_nl_df.sort_values(by='passenger_count', ascending=False, inplace=True)
